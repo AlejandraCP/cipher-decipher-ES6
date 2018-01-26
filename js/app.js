851b3dd95be1a668dd28aeaebeaeaeedc7c55a61
AlejandraCP <alejandracp710@gmail.com>
2018-01-26 03:49:13 -0500

    Agregando funciones a buttons

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 // Cifra un texto mediante el cifrado César.
 let cipher = () => {
   // Se obtiene el texto a cifrar.
-  let phrase = prompt("Escribe el texto a cifrar");
+  let input = $('#text-cipher');
+  let phrase = input.val();
   let charValue = 0;
   let cipherPhrase = "";
   // Ciclo que itera por cada elemento de phrase.
@@ -24,19 +25,22 @@ let cipher = () => {
       cipherPhrase = cipherPhrase + charValue;
     }
   }
-  return alert("El texto cifrado es: " + cipherPhrase);
+  let result = $('.result');
+  result.html(cipherPhrase);
+  // return alert("El texto cifrado es: " + cipherPhrase);
 };
 
 
 // Descifra un texto codificado por el cifrado César.
 let deCipher = () => {
   // Se obtiene el texto a descifrar
-  let phrase2 = prompt("Escribe el texto a descifrar");
+  let input = $('#text-cipher');
+  let phrase = input.val();
   let charValue2 = 0;
   let deCipherPhrase = "";
-  for (var i = 0; i < phrase2.length; i++) {
+  for (var i = 0; i < phrase.length; i++) {
     // Obtiene el valor ASCII del elemento.
-    charValue2 = phrase2.charCodeAt(i);
+    charValue2 = phrase.charCodeAt(i);
     // Si es mayor o igual a 65 y menor o igual a 90
     if (charValue2 >= 65 && charValue2 <= 90) {
       charValue2 = (charValue2 + 65 - 33) % 26 + 65;
@@ -48,9 +52,13 @@ let deCipher = () => {
       deCipherPhrase += charValue2;
     }
   }
-  return alert("El texto descifrado es: " + deCipherPhrase);
+  let result = $('.result');
+  result.html(deCipherPhrase);
 };
 
+let btnCipher = $('#cipher');
+let btnDecipher = $('#decipher');
 
-cipher();
-deCipher();
+btnCipher.on('click', cipher);
+btnDecipher.on('click', deCipher);
+
